779966befb8a1ccc7722b53e26293ca596b00268
Luiz Eduardo <Luizinformaticaeduardo@gmail.com>
2025-04-24 08:17:45 -0300

    Comit

--- ESCOLA.sql
+++ ESCOLA.sql
@@ -1,38 +1,54 @@
 CREATE DATABASE Escolaluiz;
 USE Escolaluiz;
 
-CREATE TABLE telefone
-(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    telefone VARCHAR(11) UNIQUE NOT NULL
-);
-
-CREATE TABLE endereco
-(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	endereco TEXT NOT NULL
-);
-
 CREATE TABLE alunos
 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    id_endereco INT,
-    id_telefone INT,
     nome VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    FOREIGN KEY (id_endereco) REFERENCES endereco(id)  ON DELETE CASCADE,
-    FOREIGN KEY (id_telefone) REFERENCES telefone(id)  ON DELETE CASCADE
+    cpf VARCHAR(11) UNIQUE NOT NULL
 );
 
 CREATE TABLE professores
 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    id_endereco INT,
-    id_telefone INT,
     nome VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    FOREIGN KEY (id_endereco) REFERENCES endereco(id)  ON DELETE CASCADE,
-    FOREIGN KEY (id_telefone) REFERENCES telefone(id)  ON DELETE CASCADE
+    cpf VARCHAR(11) UNIQUE NOT NULL
+);
+
+CREATE TABLE telefone_alunos
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    telefone VARCHAR(11) UNIQUE NOT NULL,
+    id_alunos INT NOT NULL,
+	FOREIGN KEY (id_alunos) REFERENCES alunos(id) ON DELETE CASCADE
+);
+
+CREATE TABLE telefone_professores
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_professores INT NOT NULL,
+    telefone VARCHAR(11) UNIQUE NOT NULL,
+	FOREIGN KEY (id_professores) REFERENCES professores(id) ON DELETE CASCADE
+);
+   
+CREATE TABLE endereco_alunos
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_alunos INT NOT NULL,
+	endereco TEXT NOT NULL,
+	rua VARCHAR(45) NOT NULL,
+    bairro TEXT NOT NULL,
+    cep VARCHAR(10),
+    FOREIGN KEY (id_alunos) REFERENCES alunos(id) ON DELETE CASCADE
+);
+CREATE TABLE endereco_professores
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_professores INT NOT NULL,
+	rua VARCHAR(45) NOT NULL,
+    bairro TEXT NOT NULL,
+    cep VARCHAR(10),
+	FOREIGN KEY (id_professores) REFERENCES professores(id) ON DELETE CASCADE
 );
 
 CREATE TABLE chamada
@@ -50,14 +66,6 @@ CREATE TABLE disciplina
     nome VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE sala
-(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    tipo VARCHAR(255) NOT NULL,
-    bloco VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE materia
 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -65,8 +73,7 @@ CREATE TABLE materia
     id_disciplina INT NOT NULL,
     id_sala INT NOT NULL,
     FOREIGN KEY (id_professores) REFERENCES professores(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_disciplina) REFERENCES disciplina(id) ON DELETE CASCADE,
-	FOREIGN KEY (id_sala) REFERENCES sala(id) ON DELETE CASCADE
+    FOREIGN KEY (id_disciplina) REFERENCES disciplina(id) ON DELETE CASCADE
 );
 
 CREATE TABLE matricula
@@ -78,9 +85,7 @@ CREATE TABLE matricula
     FOREIGN KEY (id_materia) REFERENCES materia(id) ON DELETE CASCADE
 );
 
-SELECT * FROM alunos;
+SELECT * FROM telefone_alunos;
 SELECT * FROM professores;
 SELECT * FROM chamada;
 SELECT * FROM materia;
-
-DROP DATABASE Escolaluiz;
